c8ee89822dc3aaa1de035cb6ae57d27655418fd4
samrrr <samrrr@mail.ru>
2016-09-05 11:28:15 +0300

    2213

---
+++ ConsoleApplication2/code.cpp
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int processing_1(int _in);
+
+int main()
+{
+	
+	cout <<  "Главное меню";
+
+	
+	Sleep(2342);
+}
+
+
+
+
+int processing_1(int _in)
+{
+
+}
